e51621d9bdec4b566654d7feafbf019bebf12dc0
Aqeel Nazeer <aqeelvn@gmail.com>
2017-06-08 17:05:46 +0900

    use delegate

--- app/models/comment_activity.rb
+++ app/models/comment_activity.rb
@@ -1,12 +1,9 @@
 class CommentActivity < UserActivity
   delegate :title, to: :recipe, prefix: true
   delegate :text, to: :comment
+  delegate :recipe, to: :comment
 
   def comment
     target
-  end
-
-  def recipe
-    comment.recipe
   end
 end
