5057495acb336001592835e2074a2f26c7d202c3
Aqeel Nazeer <aqeelvn@gmail.com>
2017-06-12 10:43:32 +0900

    indexing job

--- app/services/recipe_indexer.rb
+++ app/services/recipe_indexer.rb
@@ -3,15 +3,27 @@ class RecipeIndexer
     @recipe = recipe
   end
 
-  def run
-    search_index = SearchIndex.find_or_initialize_by(recipe: recipe)
-    search_index.index = recipe_index
-    search_index.save
+  def run(delay: true)
+    if delay
+      create_delayed_job
+    else
+      create_index
+    end
   end
 
   private
 
   attr_reader :recipe
+
+  def create_delayed_job
+    RecipeIndexingJob.perform_later(recipe)
+  end
+
+  def create_index
+    search_index = SearchIndex.find_or_initialize_by(recipe: recipe)
+    search_index.index = recipe_index
+    search_index.save
+  end
 
   def recipe_index
     (recipe_values + ingredient_values).join(" ")
@@ -24,5 +36,4 @@ class RecipeIndexer
   def ingredient_values
     recipe.ingredients.map(&:name)
   end
-  #Recipe.find_each { |recipe| RecipeIndexer.new(recipe).run }
 end
